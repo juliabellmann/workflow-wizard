5bba2e6e5412a1fe84d1033472ce7f48bbc83c85
juliabellmann <167864731+juliabellmann@users.noreply.github.com>
2024-11-07 22:10:30 +0100

    link local repository to GitHub

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <h2>Welcome, this is where the Workflow Wizard app will be created.</h2>
     </div>
   );
 }
