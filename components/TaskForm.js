428a859bdfa7a19edbdc95a46e98f23a21bbd591
juliabellmann <167864731+juliabellmann@users.noreply.github.com>
2024-11-09 11:46:47 +0100

    change input type of priority as specified in accetpance crtiera

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-// Definieren der optionen für priority
+// Definieren der Optionen für priority
 const priorityOptions = [
+    { id: "default", value: "", label: "Please select a priority" },
     { id: "priority1", value: "High", label: "High" },
     { id: "priority2", value: "Medium", label: "Medium" },
     { id: "priority3", value: "Low", label: "Low" },
@@ -12,6 +13,9 @@ export default function TaskForm() {
     // Initialisieren des Due Date mit aktuellem Datum
     const [dueDate, setDueDate] = useState(new Date().toISOString().split("T")[0]);
 
+    // Initialisieren des Priority States
+    const [selectedPriority, setSelectedPriority] = useState("");
+
     return (
         <>
             <label htmlFor="title"><h3>Task Name: *</h3></label>
@@ -20,23 +24,22 @@ export default function TaskForm() {
             <label htmlFor="description"><h3>Description:</h3></label>
             <input type="text" id="description" name="description" placeholder="Optional: enter a task description here" />
 
-            <label htmlFor="priority"><h3>Priority:</h3></label>
-            <section>
+            <label htmlFor="priority"><h3>Priority: *</h3></label>
+            <select
+                id="priority"
+                name="priority"
+                value={selectedPriority}
+                onChange={(e) => setSelectedPriority(e.target.value)}
+                required
+            >
                 {priorityOptions.map((option) => (
-                    <div key={option.id}>
-                    <input
-                        type="radio"
-                        id={option.id}
-                        name="priority"
-                        value={option.value}
-                        required={option.id === "priority1"}
-                    />
-                    <label htmlFor={option.id}>{option.label}</label>
-                    </div>
+                    <option key={option.id} value={option.value}>
+                        {option.label}
+                    </option>
                 ))}
-            </section>
+            </select>
 
-            <label htmlFor="dueDate"><h3>Due Date:</h3></label>
+            <label htmlFor="dueDate"><h3>Due Date: *</h3></label>
             {/* onChange: Aktualisiert den Zustand "dueDate", wenn der Nutzer ein neues Datum wählt */}
             <input type="date" id="dueDate" name="dueDate" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
 
